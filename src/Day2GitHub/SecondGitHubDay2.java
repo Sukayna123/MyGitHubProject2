8790df787d043547f4b10566a46072c9ec7e9eb8
14403 <a7la_soko@yahoo.com>
2023-01-31 21:15:54 -0500

    new changes has been done

--- src/Day2GitHub/SecondGitHubDay2.java
+++ src/Day2GitHub/SecondGitHubDay2.java
@@ -3,6 +3,7 @@ package Day2GitHub;
 public class SecondGitHubDay2 {
     public static void main(String[] args) {
         System.out.println("i am learning GitHub");
+        System.out.println("i am creating a conflict");
         System.out.println("2+2");
     }
 }
