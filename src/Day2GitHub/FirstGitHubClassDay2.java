25b7e240d2a9f1a30a5f181bf9fe69a08bb75ad5
14403 <a7la_soko@yahoo.com>
2023-01-31 19:34:07 -0500

    lets try again

--- src/Day2GitHub/FirstGitHubClassDay2.java
+++ src/Day2GitHub/FirstGitHubClassDay2.java
@@ -2,6 +2,6 @@ package Day2GitHub;
 
 public class FirstGitHubClassDay2 {
     public static void main(String[] args) {
-        System.out.println("hello Batch 7");
+        System.out.println("hello B 7");
     }
 }
